244ce7cf76b672df571a318d0d3fb04e111d9897
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-04-27 20:27:02 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,10 +7,14 @@ class PostsController < ApplicationController
   def edit
   end
 
-  def update
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+  def update 
+    @post.assign_attributes(post_params)
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
