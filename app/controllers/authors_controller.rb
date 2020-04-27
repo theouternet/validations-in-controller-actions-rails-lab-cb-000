962196e274f0b1ff1841c9190f5e2a46e790e148
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-04-27 20:15:08 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -9,7 +9,14 @@ class AuthorsController < ApplicationController
   def create
     @author = Author.create!(author_params)
 
+if @author.valid? 
+  @author.save
+  
     redirect_to author_path(@author)
+  
+else 
+  render :new 
+end 
   end
 
   private
