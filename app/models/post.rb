3aa07a87209427b1fb183cc2351ed0aafc8632c3
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-04-27 20:09:16 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true 
-  validates :category, inclusion: %w{ in: (Fiction, Non-Fiction) }
+  validates :category, inclusion: { in: %w(Fiction, Non-Fiction) }
 end
